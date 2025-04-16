5707b93628aed72d4c47ee79be00373ffa197df5
subbu <subbu@gmail.com>
2025-04-16 17:21:37 +0530

    m1

---
+++ tuesday33.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("1.Temporary\n2.Permanent\nEnter choice[1..2]:");
+	scanf("%i",&ch);
+	if(ch==1)
+	{
+		printf("subbu\n");
+		printf("c/o igate solutions\n");
+		printf("Kavali");
+	}
+	if(ch==2)
+	{
+		printf("Dno:5-5-16/A\n");
+		printf("Kukkala gunta\n");
+		printf("Kavali");
+	}
+	return 0;
+}
